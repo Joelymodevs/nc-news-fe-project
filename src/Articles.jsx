839ce396d3bf7990051419fa5ac08b10c0f0ed6f
Joel Morton <joelmortondev@gmail.com>
2023-05-31 10:51:42 +0100

    sorry

---
+++ src/Articles.jsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from "react";
+import { getArticles } from "./utils/getAllArticles";
+import { Link } from 'react-router-dom'
+import "./articles.css";
+
+const Article = () => {
+  const [articles, setArticles] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    getArticles()
+      .then((data) => {
+        setArticles(data);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setIsLoading(false);
+      });
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div>
+        <p>Loading...</p>
+      </div>
+    );
+  }
+  return (
+    <div className="articles">
+      {articles.map((article) => (
+        <Link to={`/articles/${article.article_id}`} key={article.title}>
+        <div key={article.article_id} className="article-card">
+          <img
+            src={article.article_img_url}
+            alt="article-image"
+            className="card-img"
+          />
+          <div className="card-main">
+            <h2>{article.title}</h2>
+            <p>{article.author}</p>
+            <p></p>
+            <p>Topic: {article.topic}</p>
+          </div>
+        </div>
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export default Article;
